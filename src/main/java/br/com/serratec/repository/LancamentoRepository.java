5fd659fd0665a0f719154ff878772e96c7875b77
Laiz <laiz@outlook.com>
2024-10-18 15:29:52 -0300

    Atualização do LancamentoRepository para aceitar o Page<>

--- src/main/java/br/com/serratec/repository/LancamentoRepository.java
+++ src/main/java/br/com/serratec/repository/LancamentoRepository.java
@@ -1,9 +1,11 @@
 package br.com.serratec.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.serratec.entity.LancamentoVendas;
 
 public interface LancamentoRepository extends JpaRepository<LancamentoVendas, Long>{
-
+  Page<LancamentoVendas> findAll(Pageable pageable);
 }
