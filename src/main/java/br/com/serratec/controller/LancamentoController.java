1cfb4bb6e50648e9673291558f3a86d974e87463
Laiz <laiz@outlook.com>
2024-10-18 15:41:01 -0300

    Inserção de Paginação no LancamentoController

--- src/main/java/br/com/serratec/controller/LancamentoController.java
+++ src/main/java/br/com/serratec/controller/LancamentoController.java
@@ -17,6 +17,9 @@ public class LancamentoController {
     @Autowired
     private LancamentoService lancamentoService;
 
+    @Autowired
+    private LancamentoVendasRepository lancamentoVendasRepository;
+
     @GetMapping("/{id}")
     public ResponseEntity<List<LancamentoVendasResponseDTO>> listarPorId(@PathVariable Long id) {
         List<LancamentoVendasResponseDTO> dto = lancamentoService.listarPorId(id);
@@ -27,6 +30,29 @@ public class LancamentoController {
     public ResponseEntity<LancamentoVendas> inserirLancamento(@RequestBody LancamentoVendas lancamentoVendas) {
         LancamentoVendas novoLancamento = lancamentoService.inserirLancamento(lancamentoVendas);
         return ResponseEntity.status(201).body(novoLancamento);
+    }
+
+    //Request com Paginação
+	@Operation(summary = "Lista Paginada de Vendas", description = "Retorna Lançamentos de Venda Paginadas")
+	@ApiResponses(value = { @ApiResponse(responseCode = "200",  content = {
+			@Content(schema = @Schema(implementation = LancamentoVendas.class), mediaType = "application/json") }, description = "Lancamento de venda cadastrado com sucesso"),
+			@ApiResponse(responseCode = "401", description = "Erro de autenticação"),
+			@ApiResponse(responseCode = "403", description = "Não há permissão para acessar o recurso"),
+			@ApiResponse(responseCode = "404", description = "Recurso não encontrado"),
+			@ApiResponse(responseCode = "505", description = "Exceção interna da aplicação") })
+    @GetMapping("/paginacao")
+    public ResponseEntity<Page<LancamentoVendasResponseDTO>> listarLancamentos(Pageable pageable) {
+        Page<LancamentoVendas> lancamentos = lancamentoVendasRepository.findAll(pageable);
+        Page<LancamentoVendasResponseDTO> response = lancamentos.map(lancamento -> {
+            LancamentoVendasResponseDTO dto = new LancamentoVendasResponseDTO();
+            dto.setData(lancamento.getData());
+            dto.setValor(lancamento.getValor());
+            dto.setNomeVendedor(lancamento.getVendedorAutonomo().getNome());
+            return dto});
+        
+        return ResponseEntity.ok(response);
+        //exemplo de requisição por valor decrescente no postman: 
+        //localhost:8080/lancamentos/paginacao?page=0&size=10&sort=valor,desc
     }
 }
 
